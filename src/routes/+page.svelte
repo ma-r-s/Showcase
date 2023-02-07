bca1d285a5a37e7d710b0ffc070a35cb19b6e4f2
Mario Ruiz <m.ruizs@uniandes.edu.co>
2023-02-07 03:36:01 -0500

    Threlte Skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,39 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import { Canvas, OrbitControls, T, Pass } from '@threlte/core';
+	import { degToRad } from 'three/src/math/MathUtils';
+	import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+	import { AmbientLight, Vector2 } from 'three';
+</script>
+
+<div class="bg-black min-h-screen flex flex-col items-center justify-center overflow-hidden">
+	<h1 class="text-3xl text-white font-bold m-5">Tailwind + Threlte</h1>
+
+	<div class="w-full grow">
+		<Canvas>
+			<T.AmbientLight />
+			<T.PerspectiveCamera makeDefault position={[20, 0, 10]}>
+				<OrbitControls maxPolarAngle={degToRad(90)} enableZoom={false} autoRotate />
+			</T.PerspectiveCamera><T.Mesh position.z={-4}>
+				<T.TorusGeometry args={[3, 0.2, 256, 32]} />
+				<T.MeshPhysicalMaterial color="#FF1000" />
+			</T.Mesh>
+			<T.Mesh position.z={-2}>
+				<T.TorusGeometry args={[4, 0.2, 256, 32]} />
+				<T.MeshPhysicalMaterial color="#FF00FF" />
+			</T.Mesh>
+			<T.Mesh>
+				<T.TorusGeometry args={[5, 0.2, 256, 32]} />
+				<T.MeshPhysicalMaterial color="#FF0000" />
+			</T.Mesh>
+			<T.Mesh position.z={2}>
+				<T.TorusGeometry args={[4, 0.2, 256, 32]} />
+				<T.MeshPhysicalMaterial color="#FF00FF" />
+			</T.Mesh>
+			<T.Mesh position.z={4}>
+				<T.TorusGeometry args={[3, 0.2, 256, 32]} />
+				<T.MeshPhysicalMaterial color="#FF1000" />
+			</T.Mesh>
+			<Pass pass={new UnrealBloomPass(new Vector2(256, 256), 0.3, 5, 0.0)} />
+		</Canvas>
+	</div>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import seqPreprocessor from 'svelte-sequential-preprocessor';
+import { preprocessThrelte } from '@threlte/preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
+	preprocess: seqPreprocessor([vitePreprocess(), preprocessThrelte()])
 };
 
 export default config;
