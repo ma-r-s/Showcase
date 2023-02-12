416b402fa1f15286b685f4364d38feef8a1d8946
Mario Ruiz <m.ruizs@uniandes.edu.co>
2023-02-12 06:26:58 -0500

    Capacitor initial config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import seqPreprocessor from 'svelte-sequential-preprocessor';
 import { preprocessThrelte } from '@threlte/preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		})
 	},
 	preprocess: seqPreprocessor([vitePreprocess(), preprocessThrelte()])
 };
