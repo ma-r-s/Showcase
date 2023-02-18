e699fb29fb199d761ecc19cef6e090822d1d5f3a
Mario Ruiz <m.ruizs@uniandes.edu.co>
2023-02-18 12:54:30 -0500

    Unfinished

--- src/routes/animals/+page.svelte
+++ src/routes/animals/+page.svelte
@@ -7,8 +7,13 @@
 	let drawn = false;
 	let look = true;
 
-	const inter = (a, b) => (t) => interpolate([a, b])(t);
-	const path = tweened(cow[0], {
+	const inter = (a, b) => (t) => interpolate([a, b], { optimize: 'none' })(t);
+	const catPath = tweened(cat[0], {
+		duration: 800,
+		interpolate: inter,
+		easing: sineInOut
+	});
+	const cowPath = tweened(cow[0], {
 		duration: 800,
 		interpolate: inter,
 		easing: sineInOut
@@ -26,8 +31,8 @@
 	</button>
 	<button
 		on:click={() => {
-			if (look) path.set(cow[1]);
-			else path.set(cat[0]);
+			if (look) catPath.set(cat[1]) && cowPath.set(cow[1]);
+			else catPath.set(cat[0]) && cowPath.set(cow[0]);
 			look = !look;
 		}}
 		class="rounded full p-2 bg-red-500 text-white"
@@ -35,11 +40,22 @@
 		Morph
 	</button>
 </div>
-<svg class="w-1/3" viewBox="0 0 800 600">
+<svg viewBox="0 0 1600 600">
 	{#if drawn}
 		<path
 			transition:draw={{ duration: 3000, easing: sineInOut }}
-			d={$path}
+			d={$catPath}
+			fill="none"
+			opacity="1"
+			stroke="#ffffff"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="12"
+		/>
+		<path
+			transition:draw={{ duration: 3000, easing: sineInOut }}
+			transform="translate(800, 0)"
+			d={$cowPath}
 			fill="none"
 			opacity="1"
 			stroke="#ffffff"
---
+++ src/routes/clip/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	let vol = 2;
+</script>
+
+<div class=" grow flex w-full justify-center items-center">
+	<div class="w-1/4 flex  flex-col-reverse gap-2 items-center ">
+		{#each Array(12) as _, i}
+			<button
+				on:click={() => (vol = i)}
+				class="rounded-full text-white  w-16 h-4 {vol < i ? 'bg-slate-100' : 'bg-red-500'}"
+			/>
+		{/each}
+	</div>
+	<div class="flex-col flex w-1/2">
+		<svg class="border" viewBox="0,0,400,300">
+			{#each Array(5) as _, i}
+				<line y1="300" x1="100" x2="200" stroke="white" stroke-width="2" />
+			{/each}
+		</svg>
+	</div>
+	<div class="w-1/4 flex flex-col-reverse gap-2 items-center">
+		{#each Array(12) as _, i}
+			<button
+				on:click={() => (vol = i)}
+				class="rounded-full text-white w-16 h-4 {vol < i ? 'bg-slate-100' : 'bg-red-500'}"
+			/>
+		{/each}
+	</div>
+</div>
